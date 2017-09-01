b378da9de4cc1bf225d4d72fa4be6b8026651736
David Dawson <me@daviddawson.me>
2017-09-01 17:19:24 +0100

    Update version to while publishing

---
+++ java/muon-amqp/src/main/java/io/muoncore/protocol/broadcast/AmqpBroadcastAdaptor.java
@@ -0,0 +1,16 @@
+package io.muoncore.protocol.broadcast;
+
+import io.muoncore.Discovery;
+import io.muoncore.extension.amqp.AMQPMuonTransport;
+
+/**
+ * Implement the broadcast stack within the AMQP transport
+ */
+public class AmqpBroadcast {
+
+    public AmqpBroadcast(Discovery discovery, AMQPMuonTransport transport) {
+
+
+
+    }
+}
